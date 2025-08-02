36a70d59732b39921c5d8e2c25fa28420dd39465
oldvalley <oldvalley.49@gmail.com>
2025-08-01 22:18:49 -0400

    move scripts to STALE

--- script/3_formatID.sh
+++
@@ -1,1 +0,0 @@
-sed -E 's/\b(ID|Parent)=(gene-|rna-|exon-|cds-)/\1=/g' output/MANE_mapped_target_reformatted.gff3 > output/MANE_mapped_target_ready.gff3
--- script/to_one_based_indexing.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import os
-for file in os.listdir('changes/'):
-    fp = 'changes/' + file
-    df = pd.read_csv(fp, index_col = 0)
-    df['start'] += 1
-    df.to_csv(fp)
-
